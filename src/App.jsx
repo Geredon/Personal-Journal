16f3ae97d829736755183961887f22883c182b15
Мучичка Александр <Amuchichka@ibs.ru>
2024-08-17 18:09:48 +0300

    добавление воспоминаний

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,49 @@ import Header from './components/Header/Header.jsx';
 import JournalList from './components/JournalList/JournalList.jsx';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton.jsx';
 import {JournalForm} from './components/JournalForm/JournalForm.jsx';
+import {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const INITIAL_DATA = [
+	{
+		title:'Подготовка обновления курсов',
+		date: new Date(),
+		text: 'Горные походы открывают удивительные природыне ландшафты'
+
+	},
+	{
+		title:'Поход в горы',
+		date: new Date(),
+		text: 'Горные походы открывают удивительные природыне ландшафты'
+
+	},
+	{
+		title:'Развлечься',
+		date: new Date(),
+		text: 'Горные походы открывают удивительные природыне ландшафты'
+
+	}
+];
 
 function App() {
-	const data = [
-		{
-			title:'Подготовка обновления курсов',
-			date: new Date(),
-			text: 'Горные походы открывают удивительные природыне ландшафты'
+	const [items, setItems] = useState(INITIAL_DATA);
 
-		},
-		{
-			title:'Поход в горы',
-			date: new Date(),
-			text: 'Горные походы открывают удивительные природыне ландшафты'
+	const addItem = (newItem) => {
+		setItems((oldItems) => [...oldItems, {
+			text: newItem.text,
+			title: newItem.title,
+			date: new Date(newItem.date),
+			id: uuidv4()
+		}]);
+	};
 
-		},
-		{
-			title:'Развлечься',
-			date: new Date(),
-			text: 'Горные походы открывают удивительные природыне ландшафты'
-
+	const handleSortItems = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
 		}
-	];
+	};
 
 	return (
 		<div className="app">
@@ -36,31 +57,21 @@ function App() {
 				<Header/>
 				<JournalList>
 					<JournalAddButton/>
-					<CardButton>
-						<JournalItem
-							title={data[0].title}
-							text={data[0].text}
-							date={data[0].date}
-						/>
-					</CardButton>
-					<CardButton>
-						<JournalItem
-							title={data[1].title}
-							text={data[1].text}
-							date={data[1].date}
-						/>
-					</CardButton>
-					<CardButton>
-						<JournalItem
-							title={data[2].title}
-							text={data[2].text}
-							date={data[2].date}
-						/>
-					</CardButton>
+					{items?.length && items.sort(handleSortItems).map((item) => (
+						<CardButton key={item.id}>
+							<JournalItem
+								title={item.title}
+								text={item.text}
+								date={item.date}
+							/>
+						</CardButton>
+					)
+					)}
+					{!items?.length && <p>Нет воспоминаний</p>}
 				</JournalList>
 			</LeftPanel>
 			<Body>
-				<JournalForm/>
+				<JournalForm onSubmit={addItem}/>
 			</Body>
 		</div>
 	);
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,28 +1,22 @@
-import {useState} from 'react';
 import './JournalForm.css';
 import Button from '../Button/Button.jsx';
 
 
-export const JournalForm = () => {
-	const [inputData, setInputData] = useState('');
-
-	const handleInput = (e) => {
-		setInputData(e.target.value);
-	};
+export const JournalForm = ({onSubmit}) => {
 
 	const addJournalItem = (e) => {
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
 		e.preventDefault();
-		console.log(formProps);
+		onSubmit(formProps);
 	};
 
 	return (
 		<form className="journal-form" onSubmit={addJournalItem}>
 			<input type="text" name="title"/>
 			<input type="date" name="date"/>
-			<input type="text" name="tag" value={inputData} onChange={handleInput}/>
-			<textarea name="post" id="" cols="30" rows="10"></textarea>
+			<input type="text" name="tag"/>
+			<textarea name="text" id="" cols="30" rows="10"></textarea>
 			<Button title="Сохранить" onclick={() => console.log('нажали')}/>
 		</form>
 
